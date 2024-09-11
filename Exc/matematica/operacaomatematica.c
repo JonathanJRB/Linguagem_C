6f99eaa5fec54ef36e32bdaf2387d45f98494fde
jonathanjrb <jonathanrodriguesbarbosa@souunisuam.com.br>
2024-09-11 14:58:01 +0100

    matematica

---
+++ Exc/matematica/operacaomatematica.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void	main()
+{
+	//definindo variaveis
+	int	a = 5, b = 3;
+
+	//soma
+	printf("\nA soma de a e b = %d", a + b);
+
+	//subtracao
+	printf("\n A subtracao de a e b = %d", a - b);
+
+	//divisao
+	printf("\n A divisao de a e b = %d", a / b);
+
+	//multiplicacao
+	printf("\n A multiplicacao de a e b = %d", a * b);
+
+	//resto da divisao
+	printf("\n O resto da divisao de a e b = %d", a % b);
+
+
+
+}
